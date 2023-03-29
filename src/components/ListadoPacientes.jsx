36ae5f50b0ff8350fb042d11fba9a1d813dce3de
Ariel Cabeza <cervanteslibch@gmail.com>
2023-03-29 01:26:10 -0300

    Estilando Header con Tailwind

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListadoPacientes() {
+  return (
+    <h3>ListadoPacientes</h3>
+  )
+}
+
+export default ListadoPacientes
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.jsx"],//de esta forma aplicamos tailwind de forma recursiva a toda la app
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
